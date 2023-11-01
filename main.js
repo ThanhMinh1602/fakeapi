777ff8633245cd6a16a853c881d0470acc84dd8e
ThanhMinh1602 <ntminh16201@gmail.com>
2023-11-01 16:29:38 +0700

    mamama

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const bodyParser = require('body-parser'); // Import body-parser
 const { v4: uuidv4 } = require('uuid');
 const jsonServer = require('json-server');
 
 const server = express();
 const port = process.env.PORT || 3000;
+
+server.use(bodyParser.json()); // Use body-parser for JSON parsing
 
 // Phần mềm trung gian
 server.use(jsonServer.defaults());
@@ -14,6 +17,7 @@ server.use(jsonServer.defaults());
 // Thiết lập bộ định tuyến Máy chủ JSON
 const jsonServerRouter = jsonServer.router('db.json');
 server.use('/api', jsonServerRouter);
+
 
 // Thiết lập Multer để upload file
 const storage = multer.diskStorage({
@@ -74,7 +78,45 @@ server.post('/upload', upload.single('image'), (req, res) => {
     res.status(400).send('No file uploaded');
   }
 });
+// API đăng nhập http://localhost:3000/login
+server.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const authData = JSON.parse(fs.readFileSync('auth.json', 'utf8'));
 
+  const user = authData.users.find((u) => u.username === username && u.password === password);
+
+  if (user) {
+    // User authenticated
+    res.status(200).json({ message: 'Login successful' });
+  } else {
+    // Authentication failed
+    res.status(401).json({ message: 'Login failed' });
+  }
+});
+
+// API đăng ký http://localhost:3000/register
+server.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  const authData = JSON.parse(fs.readFileSync('auth.json', 'utf8'));
+
+  // Check if the username is already taken
+  const userExists = authData.users.find((u) => u.username === username);
+
+  if (userExists) {
+    res.status(400).json({ message: 'Username already exists' });
+  } else {
+    const newUser = {
+      username,
+      password,
+    };
+
+    authData.users.push(newUser);
+
+    fs.writeFileSync('auth.json', JSON.stringify(authData, null, 2));
+
+    res.status(201).json({ message: 'Registration successful' });
+  }
+});
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
